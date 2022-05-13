ce255cf5e8f58506bb6288e3b986184afdd3c64b
Noah Saso <noahsaso@gmail.com>
2022-05-13 03:34:21 -0700

    Redesigned API.

--- dist/components/BaseModal.d.ts
+++ dist/components/BaseModal.d.ts
@@ -3,6 +3,7 @@ export interface ModalClassNames {
     modalContent?: string;
     modalOverlay?: string;
     modalHeader?: string;
+    modalSubheader?: string;
     modalCloseButton?: string;
     walletList?: string;
     wallet?: string;
@@ -10,6 +11,7 @@ export interface ModalClassNames {
     walletInfo?: string;
     walletName?: string;
     walletDescription?: string;
+    textContent?: string;
 }
 export interface BaseModalProps {
     isOpen: boolean;
@@ -20,4 +22,5 @@ export interface BaseModalProps {
     closeIcon?: ReactNode;
 }
 export declare const BaseModal: FunctionComponent<BaseModalProps>;
+export declare const ModalSubheader: import("styled-components").StyledComponent<"div", any, {}, never>;
 //# sourceMappingURL=BaseModal.d.ts.map
--- dist/components/WalletManagerContext.d.ts
+++ dist/components/WalletManagerContext.d.ts
@@ -2,29 +2,37 @@ import { IClientMeta } from "@walletconnect/types";
 import React, { FunctionComponent, ReactNode } from "react";
 import { ModalClassNames } from ".";
 import { Wallet, WalletClient } from "./common";
+interface ConnectedWallet {
+    wallet: Wallet;
+    client: WalletClient;
+}
 export declare const WalletManagerContext: React.Context<{
-    getWalletClient: () => Promise<WalletClient | undefined>;
-    setDefaultWalletId: (id: string | undefined) => void;
-    connectedWalletId?: string | undefined;
+    connect: () => void;
+    disconnect: () => void | Promise<void>;
+    connectedWallet?: ConnectedWallet | undefined;
+    connectionError?: unknown;
+    isMobileWeb: boolean;
 } | null>;
 export declare enum Event {
-    ModalClose = "modal_close",
-    QrModalClose = "qr_modal_close",
     WalletSelected = "wallet_selected"
 }
 interface WalletManagerProviderProps {
     wallets: Wallet[];
+    enableKeplr: (wallet: Wallet, walletClient: WalletClient) => Promise<void> | void;
     children: ReactNode;
     classNames?: ModalClassNames;
     closeIcon?: ReactNode;
-    defaultWalletId?: string | undefined;
+    preselectedWalletId?: string | undefined;
     clientMeta?: IClientMeta;
+    attemptAutoConnect?: boolean;
 }
 export declare const WalletManagerProvider: FunctionComponent<WalletManagerProviderProps>;
 export declare const useWalletManager: () => {
-    getWalletClient: () => Promise<WalletClient | undefined>;
-    setDefaultWalletId: (id: string | undefined) => void;
-    connectedWalletId?: string | undefined;
+    connect: () => void;
+    disconnect: () => void | Promise<void>;
+    connectedWallet?: ConnectedWallet | undefined;
+    connectionError?: unknown;
+    isMobileWeb: boolean;
 };
 export {};
 //# sourceMappingURL=WalletManagerContext.d.ts.map
