4d4e084fe637c99b505f6c1d2158bbb08c062b5c
Noah Saso <noahsaso@gmail.com>
2022-05-13 12:43:29 -0700

    Fixed modals not showing up correctly and added way to forcibly disconnect WalletConnect if enable keplr modal gets stuck.

--- dist/components/EnablingKeplrModal.d.ts
+++ dist/components/EnablingKeplrModal.d.ts
@@ -2,5 +2,6 @@ import { FunctionComponent, ReactNode } from "react";
 import { BaseModalProps } from "./BaseModal";
 export declare const EnablingKeplrModal: FunctionComponent<BaseModalProps & {
     renderContent?: () => ReactNode;
+    reset: () => void;
 }>;
 //# sourceMappingURL=EnablingKeplrModal.d.ts.map
